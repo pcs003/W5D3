e9f31146f626cb3c0bc8548015d29b5fe1ec73cc
Lijun <gan.lijun.glj@gmail.com>
2021-01-27 14:14:20 -0500

    created all class for each table

--- AAQuestions/questions.rb
+++ AAQuestions/questions.rb
@@ -11,12 +11,12 @@ class QuestionsDBConnection < SQLite3::Database
   end
 end
 
-class Users
+class User
     attr_accessor :id, :fname, :lname
 
     def self.all
         data = QuestionsDBConnection.instance.execute("SELECT * FROM users")
-        data.map { |datum| Users.new(datum) }
+        data.map { |datum| User.new(datum) }
       end
 
     def self.find_by_id(id)
@@ -29,7 +29,7 @@ class Users
                 id = ?
         SQL
         return nil unless user.length > 0
-        Users.new(user.first)
+        User.new(user.first)
     end
 
     def self.find_by_name(fname, lname)
@@ -42,7 +42,7 @@ class Users
                 fname = ? AND lname = ?
         SQL
         return nil unless user.length > 0
-        Users.new(user.first)
+        User.new(user.first)
     end
 
     def initialize(user_info)
@@ -54,43 +54,174 @@ class Users
 
 end
 
-class Questions
+class Question
     attr_accessor :id, :title, :body, :associated_author
 
     def self.all
         data = QuestionsDBConnection.instance.execute("SELECT * FROM questions")
-        data.map { |datum| Questions.new(datum) }
+        data.map { |datum| Question.new(datum) }
     end
 
     def self.find_by_id(id)
-        user = QuestionsDBConnection.instance.execute(<<-SQL, id)
+
+        question = QuestionsDBConnection.instance.execute(<<-SQL, id)
+
             SELECT
                 *
             FROM
-                users
+                questions
             WHERE
                 id = ?
         SQL
-        return nil unless user.length > 0
-        Users.new(user.first)
+        return nil unless question.length > 0
+        Question.new(question.first)
     end
 
-    def self.find_by_name(fname, lname)
-        user = QuestionsDBConnection.instance.execute(<<-SQL, fname, lname)
+    def self.find_by_author_id(author_id)
+        author = User.find_by_id(id)
+        raise '#{self} is not in database' unless author
+        questions = QuestionsDBConnection.instance.execute(<<-SQL, author.id)
             SELECT
                 *
             FROM
-                users
+                questions
             WHERE
-                fname = ? AND lname = ?
+                associated_author = ?
         SQL
-        return nil unless user.length > 0
-        Users.new(user.first)
-    end
+        return nil unless questions.length > 0
+        questions.map {|question| Question.new(question) }
+    end 
 
-    def initialize(user_info)
-        @id = user_info['id']
-        @fname = user_info['fname']
-        @lname = user_info['lname']
+    def initialize(question_info)
+        @id = question_info['id']
+        @title = question_info['title']
+        @body = question_info['body']
+        @associated_author = question_info['associated_author']
     end
 end
+
+
+class Question_Follow
+    attr_accessor :id, :users_id, :questions_id
+
+    def self.all
+        data = QuestionsDBConnection.instance.execute("SELECT * FROM question_follows")
+        data.map { |datum| Question_Follow.new(datum) }
+    end
+
+    def self.find_by_id(id)
+        question_follow = QuestionsDBConnection.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                question_follows
+            WHERE
+                id = ?
+        SQL
+        return nil unless question_follow.length > 0
+        Question_Follow.new(question_follow.first)
+    end
+
+    def initialize(question_follow_info)
+        @id = question_follow_info['id']
+        @users_id = question_follow_info['users_id']
+        @questions_id = question_follow_info['questions_id']
+    end
+end
+
+class Reply
+
+    attr_accessor :id, :body, :users_id, :questions_id, :parent_reply_id 
+
+    def self.all
+        data = QuestionsDBConnection.instance.execute("SELECT * FROM replies")
+        data.map { |datum| Reply.new(datum) }
+    end
+
+    def self.find_by_id(id)
+        reply = QuestionsDBConnection.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                replies
+            WHERE
+                id = ?
+        SQL
+        return nil unless reply.length > 0
+        Reply.new(reply.first)
+    end
+
+    def self.find_by_user_id(id)
+        author = User.find_by_id(id)
+        raise '#{self} is not in database' unless author
+        replies = QuestionsDBConnection.instance.execute(<<-SQL, author.id)
+            SELECT
+                *
+            FROM
+                replies
+            WHERE
+                users_id = ?
+        SQL
+        return nil unless replies.length > 0
+        replies.map {|reply| Reply.new(reply) }
+    end 
+
+
+    def self.find_by_question_id(id)
+        question = Question.find_by_id(id)
+        raise '#{self} is not in database' unless question
+        replies = QuestionsDBConnection.instance.execute(<<-SQL, question.id)
+            SELECT
+                *
+            FROM
+                replies
+            WHERE
+                questions_id = ?
+        SQL
+        return nil unless replies.length > 0
+        replies.map {|reply| Reply.new(reply) }
+    end 
+
+    def initialize(reply_info)
+        @id = reply_info['id']
+        @body = reply_info['body']
+        @users_id = reply_info['users_id']
+        @questions_id = reply_info['questions_id']
+        @parent_reply_id = reply_info['parent_reply_id']
+    end
+
+end
+
+
+class Question_Like
+
+    attr_accessor :id, :users_id, :questions_id
+
+    def self.all
+        data = QuestionsDBConnection.instance.execute("SELECT * FROM question_likes")
+        data.map { |datum| Question_Like.new(datum) }
+    end
+
+    def self.find_by_id(id)
+        question_like = QuestionsDBConnection.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                question_likes
+            WHERE
+                id = ?
+        SQL
+        return nil unless question_like.length > 0
+        Question_Like.new(question_like.first)
+    end
+
+
+    def initialize(question_like_info)
+        @id = question_like_info['id']
+        @users_id = question_like_info['users_id']
+        @questions_id = question_like_info['questions_id']
+    end
+end
+
+
+
